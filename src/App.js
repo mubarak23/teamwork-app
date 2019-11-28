b13ce8b41607cc74f4d57b0898986d49512342b7
mubarak23 <mubarakaminu340@gmail.com>
2019-11-28 19:26:18 +0100

    ft(setup):create register form[finish]

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar'
 import Login from './components/auth/Login';
+//import Register from './components/auth/Register';
 import './App.css';
 
 function App() {
