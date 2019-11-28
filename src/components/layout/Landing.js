306af2124754955b5c1923109e48aaff6531f0f2
mubarak23 <mubarakaminu340@gmail.com>
2019-11-28 17:37:26 +0100

    ft(setup): create navbar and landing page component[progress]

---
+++ src/components/layout/Landing.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+ const Landing =() => {
+    return (
+        <section className="landing">
+            <div className="dark-overlay">
+                <div className="lading-inner">
+                    <h2 className="x-large">Team Work</h2>
+                        <p className="lead">An Internal social media platform that allow 
+                        company interact and bond between themselves</p>
+                        <div className="buttons">
+                            <a href="#register">Register</a>
+                            || <a href="#login">Login</a>
+                        </div>
+                </div>
+            </div>
+        </section>
+        
+    )
+}
+
+export default Landing;
