c0422776f624f9f5ea56a3a20807f6a46c14dc4d
mubarak23 <mubarakaminu340@gmail.com>
2019-11-28 18:05:58 +0100

    ft(setup)login form structure created successfully[finish]

---
+++ src/components/auth/Login.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './style.css'
+class Login extends Component {
+
+    render() {
+        return (
+          <div>
+            <form>
+                <div className="imgcontainer">
+                    <img scr="img_avatar2.png" alt="Avatar" className="Avater" />
+                </div>
+                <div className="container">
+                    <label for="email">
+                        <b>Email</b>
+                    </label>
+                    <input type="text"
+                    required
+                    placeholder="Enter Your Email" name="email" />
+                
+                    <label for="password">
+                        <b>Password</b>
+                    </label>
+                    <input type="password" 
+                       placeholder="Enter Your Password"
+                       required 
+                    />
+                <button type="submit">Login</button>
+                <label>
+                    <input type="checkbox" checked="checked" name="remember" />
+                    Remember Me
+                </label>
+                </div>
+                <div className="container">
+                    <button type="button" className="cancelbtn">Cancel</button>
+                </div>
+                <span className="rpw">
+                    <a href="#forgotpassword">Forgot Password</a>
+                </span>  
+            </form>  
+          </div>      
+        );
+    }
+}
+
+export default Login;   
