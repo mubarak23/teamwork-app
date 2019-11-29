b8b8a7e858690e127b598cac804a401c71906c66
mubarak23 <mubarakaminu340@gmail.com>
2019-11-29 10:31:53 +0100

    ft(register):link state value with form fields[finish]

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,126 +1,152 @@
-import React, { Component, Fragment } from 'react';
-import './style.css'
+import React, { Component, Fragment } from "react";
+import "./style.css";
 
 class Register extends Component {
-    
-    onFildChange(e){
-        e.preventDefault();
-    }
-    onRegisterSubmit(e){
-      e.preventDefault();
-    }
-    render() {
-        return (
-        <Fragment>
-            <h1 className="large text-primary">Signup</h1>
-            <p className="lead">
-                <i className="fas fa-user">Create Your Account</i>
-            </p>
-            <form className="form" onSubmit={this.onRegisterSubmit} >
-                <div>
-                    <label for="email">
-                      Email Address  
-                    </label>
-                    <input type="text" 
-                      onChange={ this.onFildChange}
-                      className="form-control"
-                      name="email"
-                      placeholder="Email Address"
-                      required />
-                </div>
-                <div>
-                    <label for="password">
-                     Password  
-                    </label>
-                    <input type="text" 
-                      onChange={this.onFildChange}
-                      className="form-control"
-                      name="password"
-                      placeholder="Enter Password"
-                      required />
-                </div>
-                <div>
-                    <label for="first Name">
-                      First Name  
-                    </label>
-                    <input type="text" 
-                      onChange={ this.onFildChange}
-                      className="form-control"
-                      name="firstName"
-                      placeholder="Enter FirstName"
-                      required />
-                </div>
-                <div>
-                    <label for="LastName">
-                      Last Name  
-                    </label>
-                    <input type="text" 
-                      onChange={ this.onFildChange}
-                      className="form-control"
-                      name="LastName"
-                      placeholder="Enter LastName"
-                      required />
-                </div>
-                <div>
-                    <label for="gender">
-                     Gender 
-                    </label>
-                    <input type="text" 
-                      onChange={ this.onFildChange}
-                      className="form-control"
-                      name="Gender"
-                      required /> 
-                </div>
-                <div>
-                    <label for="job Role">
-                    Job Role  
-                    </label>
-                    <input type="text" 
-                      onChange={ this.onFildChange}
-                      className="form-control"
-                      name="department"
-                      required /> 
-                </div>
-                <div>
-                    <label for="Department">
-                      Department 
-                    </label>
-                    <input type="text" 
-                      onChange={ this.onFildChange}
-                      className="form-control"
-                      name="department"
-                      required /> 
-                </div>
-                <div>
-                    <label for="Avater URL">
-                      Avater URL  
-                    </label>
-                    <input type="text" 
-                      onChange={ this.onFildChange}
-                      className="form-control"
-                      name="avater_url"
-                      placeholder="Avater URL"
-                      required />
-                </div>
-                <div>
-                    <label for="Address">
-                         Address  
-                    </label>
-                    <input type="text" 
-                      onChange={ this.onFildChange}
-                      className="form-control"
-                      name="address"
-                      placeholder="Home Address"
-                      required />
-                </div>
-                <input type="submit" className="btn btn-primary" value="Register" />
-            </form>
-            <p className="">
-            Already Have An Account? <a href="#login">Login</a>
-            </p>
-        </Fragment>            
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      firstName: "",
+      LastName: "",
+      jobrole: "",
+      gender: "",
+      department: "",
+      avater_url: "",
+      address: ""
+    };
+    this.onFildChange = this.onFildChange.bind(this);
+    this.onRegisterSubmit = this.onRegisterSubmit.bind(this);
+  }
+  onFildChange(e) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+  onRegisterSubmit(e) {
+    e.preventDefault();
+  }
+  render() {
+    return (
+      <Fragment>
+        <h1 className="large text-primary">Signup</h1>
+        <p className="lead">
+          <i className="fas fa-user">Create Your Account</i>
+        </p>
+        <form className="form" onSubmit={this.onRegisterSubmit}>
+          <div>
+            <label for="email">Email Address</label>
+            <input
+              type="text"
+              value={this.state.email}
+              onChange={this.onFildChange}
+              className="form-control"
+              name="email"
+              placeholder="Email Address"
+              required
+            />
+          </div>
+          <div>
+            <label for="password">Password</label>
+            <input
+              type="text"
+              value={this.state.password}
+              onChange={this.onFildChange}
+              className="form-control"
+              name="password"
+              placeholder="Enter Password"
+              required
+            />
+          </div>
+          <div>
+            <label for="first Name">First Name</label>
+            <input
+              type="text"
+              value={this.state.firstName}
+              onChange={this.onFildChange}
+              className="form-control"
+              name="firstName"
+              placeholder="Enter FirstName"
+              required
+            />
+          </div>
+          <div>
+            <label for="LastName">Last Name</label>
+            <input
+              type="text"
+              value={this.state.LastName}
+              onChange={this.onFildChange}
+              className="form-control"
+              name="LastName"
+              placeholder="Enter LastName"
+              required
+            />
+          </div>
+          <div>
+            <label for="gender">Gender</label>
+            <input
+              type="text"
+              value={this.state.gender}
+              onChange={this.onFildChange}
+              className="form-control"
+              name="gender"
+              required
+            />
+          </div>
+          <div>
+            <label for="job Role">Job Role</label>
+            <input
+              type="text"
+              value={this.state.jobrole}
+              onChange={this.onFildChange}
+              className="form-control"
+              name="jobrole"
+              required
+            />
+          </div>
+          <div>
+            <label for="Department">Department</label>
+            <input
+              type="text"
+              value={this.state.department}
+              onChange={this.onFildChange}
+              className="form-control"
+              name="department"
+              required
+            />
+          </div>
+          <div>
+            <label for="Avater URL">Avater URL</label>
+            <input
+              type="text"
+              value={this.state.avater_url}
+              onChange={this.onFildChange}
+              className="form-control"
+              name="avater_url"
+              placeholder="Avater URL"
+              required
+            />
+          </div>
+          <div>
+            <label for="Address">Address</label>
+            <input
+              type="text"
+              value={this.state.address}
+              onChange={this.onFildChange}
+              className="form-control"
+              name="address"
+              placeholder="Home Address"
+              required
+            />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Register" />
+        </form>
+        <p className="">
+          Already Have An Account? <a href="#login">Login</a>
+        </p>
+      </Fragment>
+    );
+  }
 }
 
 export default Register;
