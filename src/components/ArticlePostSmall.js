25f5c1c32ee50b668eb0f712731f82d34b818bbf
mubarak23 <mubarakaminu340@gmail.com>
2019-12-25 09:57:06 +0100

    ft(articleList)connect feed props to small article list[progress]

--- src/components/ArticlePostSmall.js
+++ src/components/ArticlePostSmall.js
@@ -1,10 +1,46 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import Comment from "./Comment";
+import Notification from "./Notification";
+import { commentPost } from "../store/actions/articleAction";
 
 class ArticlePostSmall extends Component {
+  state = {
+    commentIsHidden: false,
+    isVisible: false
+  }
+  toggleCommentContainer = () =>{
+    this.setState({
+      commentIsHidden: !this.state.commentIsHidden
+    })
+  }
+  handlecommentPost = (event, id, comment) =>{
+    event.preventDefault();
+    this.props.commentPost(id, comment);
+    this.setState({
+      isVisible: !this.state.isVisible
+    })
+    setTimeout(() => {
+      this.setState({
+        isVisible: !this.state.isVisible
+      })
+    }, 300);
+  }
   render() {
-    return <div></div>;
+    let commentState = 'none';
+    let articleState = 'none';
+    if(this.state.commentIsHidden){
+      commentState = 'block'
+    }
+    const { id, title, article, authorId } = this.props
+    let smallArticle = article;
+    if(smallArticle.length > 320){
+      articleState = 'block';
+      smallArticle = article.split('', 320).join()
+    }
+    return <div>
+      
+    </div>;
   }
 }
 
